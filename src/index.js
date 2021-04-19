efdb7fd1569abd13e6520297d074cef410f69efc
thedat07 <49479547+thedat07@users.noreply.github.com>
2021-04-19 21:05:42 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css'; 
-import App from './movies/index';
+import App from './corona/index';
 import reportWebVitals from './reportWebVitals';
 
 
